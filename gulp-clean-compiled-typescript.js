73c7dce99d3f483adca33d254c8bd4cf6c272c1c
Gjermund Bjaanes <bjaanes@gmail.com>
2016-09-13 18:39:48 +0200

    Add support for .tsx files. Closes #1

--- gulp-clean-compiled-typescript.js
+++ gulp-clean-compiled-typescript.js
@@ -3,9 +3,11 @@ var del = require('del');
 
 function gulpCleanCompiledTypescript() {
     return through.obj(function(file, encoding, callback) {
-        if (/.ts$/.test(file.path)) {
-            var compiledFile = file.path.replace(/.ts$/, '.js');
-            var mapFile = file.path.replace(/.ts$/, '.js.map');
+        var regex = /.(ts|tsx)$/;
+
+        if (regex.test(file.path)) {
+            var compiledFile = file.path.replace(regex, '.js');
+            var mapFile = file.path.replace(regex, '.js.map');
 
             del.sync([compiledFile, mapFile]);
         }
